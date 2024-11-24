19f667308af94f3512c7a3726cdbe268e849161d
olha_sydorchuk <157120293+Oolha@users.noreply.github.com>
2024-11-24 14:45:37 +0100

    did routes and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import './App.css';
-import { Modal } from './components/Modal/Modal';
+
+import { lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import SharedLayout from './components/SharedLayout/SharedLayout';
+
+const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
+const SignUpPage = lazy(() => import('./pages/SignUpPage/SignUpPage'));
+const SignInPage = lazy(() => import('./pages/SignInPage/SignInPage'));
+const TrackerPage = lazy(() => import('./pages/TrackerPage/TrackerPage'));
 
 const App = () => {
-  return <div>
-    <Modal/>
-  </div>;
+  return (
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="signup" element={<SignUpPage />} />
+        <Route path="signin" element={<SignInPage />} />
+        <Route path="tracker" element={<TrackerPage />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
