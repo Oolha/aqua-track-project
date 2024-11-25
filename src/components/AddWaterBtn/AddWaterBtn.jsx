7f67c86cf39fd26307d059bf3c89898e062e7e5d
olga-taranenko <157386111+olga-taranenko@users.noreply.github.com>
2024-11-25 10:58:42 +0200

    add WaterMainInfo in TrackerPage

--- src/components/AddWaterBtn/AddWaterBtn.jsx
+++ src/components/AddWaterBtn/AddWaterBtn.jsx
@@ -1,7 +1,11 @@
 import css from './AddWaterBtn.module.css';
 
 const AddWaterBtn = () => {
-  return <button className={css.button}>Add water</button>;
+  return (
+    <button className={css.button}>
+      <p>Add water</p>
+    </button>
+  );
 };
 
 export default AddWaterBtn;
