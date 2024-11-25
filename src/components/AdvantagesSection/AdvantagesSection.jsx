05b45c956b5c5a271125006a3431a5d471099cfd
unknown <tmgrigorash@gmail.com>
2024-11-26 01:22:57 +0200

    created-two-components

---
+++ src/components/AdvantagesSection/AdvantagesSection.jsx
@@ -0,0 +1,37 @@
+import styles from './AdvantagesSection.module.css';
+
+function AdvantagesSection() {
+  return (
+    <div className={styles.fontSection}>
+      <div className={styles.listStylesImg}>
+        <div className={styles.avatarGroup}>
+          <img
+            className={styles.avatar}
+            src="../../assets/images/MaleMemojis3.jpg"
+            // alt="Avatar 1"
+          />
+          <img
+            className={styles.avatar}
+            src="../../assets/images/MaleMemojis2.jpg"
+            // alt="Avatar 2"
+          />
+          <img
+            className={styles.avatar}
+            src="../../assets/images/MaleMemojis1.jpg"
+            // alt="Avatar 3"
+          />
+        </div>
+        <p className={styles.textParagraph}>
+          Our <span className={styles.happy}>happy</span> customers
+        </p>
+      </div>
+      <ul className={styles.listBtn}>
+        <li className={styles.listItemHabit}>Habit drive</li>
+        <li className={styles.listItemView}>View statistics</li>
+        <li className={styles.listItemPersonal}>Personal rate setting</li>
+      </ul>
+    </div>
+  );
+}
+
+export default AdvantagesSection;
