0ef3e68ad536ee92a5b0352f9e463ce928a74ff7
Unn2007 <unn2007@i.ua>
2024-11-25 14:36:13 +0200

    Update calendar.js

--- src/utils/calendar.js
+++ src/utils/calendar.js
@@ -6,10 +6,22 @@ function getPreviousMonth(initalDate,increase=true) {
     const date = new Date(initalDate);
     (increase) ? date.setMonth(date.getMonth() + 1):date.setMonth(date.getMonth() - 1)
     const year = date.getFullYear();
-    const monthName = monthNames[date.getMonth()]; // Отримуємо назву місяця
+    const monthName = monthNames[date.getMonth()]; 
 
   return { year, monthName, date };
     
+}
+
+function getDaysInMonthFromDate(initialDate) {
+  
+  const date = new Date(initialDate);
+
+ 
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1; 
+
+  
+  return new Date(year, month, 0).getDate();
 }
 
 console.log(getPreviousMonth(new Date(),false))
