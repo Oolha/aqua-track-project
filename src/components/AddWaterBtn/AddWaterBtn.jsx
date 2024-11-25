2a587962e9fb26937d98126b5157a3efb40a5a85
olga-taranenko <157386111+olga-taranenko@users.noreply.github.com>
2024-11-25 11:04:03 +0200

    add icon plus

--- src/components/AddWaterBtn/AddWaterBtn.jsx
+++ src/components/AddWaterBtn/AddWaterBtn.jsx
@@ -1,8 +1,10 @@
 import css from './AddWaterBtn.module.css';
+import { Icon } from '../Icon/Icon';
 
 const AddWaterBtn = () => {
   return (
     <button className={css.button}>
+      <Icon id="icon-plus" size={24} className={css.icon} />
       <p>Add water</p>
     </button>
   );
