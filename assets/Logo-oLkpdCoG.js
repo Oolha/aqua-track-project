8920279764a7e3c5a7cd95faf0fd331fc82ecdb7
Oolha <157120293+Oolha@users.noreply.github.com>
2024-11-27 12:00:02 +0000

    Deploying to gh-pages from @ Oolha/aqua-track-project@12dd2dcc7b39e253017c7fc28e2ef75f5036453e 🚀

--- assets/Logo-oLkpdCoG.js
+++
@@ -1,2 +0,0 @@
-import{j as s}from"./index-CzfZ4SBd.js";const c="/assets/sprite-BRLxXkIj.svg",g=({id:t,className:r,size:o})=>s.jsx("svg",{className:r,height:o,width:o,children:s.jsx("use",{href:c+"#"+t})}),n="_logo_139tt_1",e={logo:n},h=()=>s.jsx("h3",{className:e.logo,children:"AquaTrack"});export{g as I,h as L};
-//# sourceMappingURL=Logo-oLkpdCoG.js.map
