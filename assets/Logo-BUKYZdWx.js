9794598bbaad247b706e364e2ff9607fd000d040
GrTatiana <156026369+GrTatiana@users.noreply.github.com>
2024-11-27 20:48:47 +0000

    Deploying to gh-pages from @ Oolha/aqua-track-project@8b4cfbc5017fbffb3f6267634b5d05c3f0b8490d 🚀

--- assets/Logo-BUKYZdWx.js
+++
@@ -1,2 +0,0 @@
-import{j as s}from"./index-BcoenqBu.js";const c="/assets/sprite-NbWsz_jr.svg",g=({id:t,className:r,size:o})=>s.jsx("svg",{className:r,height:o,width:o,children:s.jsx("use",{href:c+"#"+t})}),n="_logo_139tt_1",e={logo:n},h=()=>s.jsx("h3",{className:e.logo,children:"AquaTrack"});export{g as I,h as L,c as a};
-//# sourceMappingURL=Logo-BUKYZdWx.js.map
