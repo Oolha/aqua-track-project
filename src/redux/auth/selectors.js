b112ce2dde1ff2c96bfd11fee42947ae907c3cc3
olha_sydorchuk <157120293+Oolha@users.noreply.github.com>
2024-12-01 14:35:24 +0100

    auth is done

--- src/redux/auth/selectors.js
+++ src/redux/auth/selectors.js
@@ -1,3 +1,3 @@
-export const selectIsLoggedIn = (state) => {
-  return state.auth.isLoggedIn;
-};
+export const selectIsLoggedIn = (state) => state.auth.isLoggedIn;
+export const selectAuthUser = (state) => state.auth.user;
+export const selectAuthError = (state) => state.auth.error;
