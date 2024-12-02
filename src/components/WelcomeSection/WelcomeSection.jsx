dfc397af8b6e6fea13619fdd34b7b7e2d52dceaa
olha_sydorchuk <157120293+Oolha@users.noreply.github.com>
2024-12-02 10:06:02 +0100

    Update WelcomeSection.jsx

--- src/components/WelcomeSection/WelcomeSection.jsx
+++ src/components/WelcomeSection/WelcomeSection.jsx
@@ -10,17 +10,10 @@ function WelcomeSection() {
         <h2 className={styles.subtitle}>Record daily water intake and track</h2>
         <h1 className={`${styles.title} title`}>Water consumption tracker</h1>
         <div className={styles.links}>
-<<<<<<< HEAD
           <Link to="/signup" className={`${styles.linkGreen} linkGreen`}>
             Try Tracker
           </Link>
           <Link to="/signin" className={`${styles.linkWhite} linkWhite`}>
-=======
-          <Link to="/signup" className={styles.tryTrackerBtn}>
-            Try Tracker
-          </Link>
-          <Link to="/signin" className={styles.signInBtn}>
->>>>>>> main
             Sign In
           </Link>
         </div>
