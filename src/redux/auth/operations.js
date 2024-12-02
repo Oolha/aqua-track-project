43eedbbee834aca0c3d6ceeb47beaeb24efb8199
Unn2007 <unn2007@i.ua>
2024-12-02 22:35:09 +0200

    Update operations.js

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 export const instance = axios.create({
   baseURL: 'https://aqua-track-project-back.onrender.com/',
-  withCredentials: true,
+  // withCredentials: true,
 });
 
 const setHeaders = (token) => {
