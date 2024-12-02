15d54baecb568c9204e6b7c383f52cecb93e65d4
Oolha <157120293+Oolha@users.noreply.github.com>
2024-12-02 22:41:26 +0000

    Deploying to gh-pages from @ Oolha/aqua-track-project@0c4ee973162829923278733952e2edfd66463990 🚀

---
+++ assets/Logo-Cl8190FI.js
@@ -0,0 +1,2 @@
+import{j as o}from"./index-D2khHk10.js";const s="_logo_1cqim_2",c={logo:s},t=()=>o.jsx("h3",{className:c.logo,children:"AquaTrack"});export{t as L};
+//# sourceMappingURL=Logo-Cl8190FI.js.map
