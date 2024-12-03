048cc45da21f8365f738dd9967be3db81a5f82a4
Zubaha <zubaha4052@gmail.com>
2024-12-03 01:46:45 +0100

    add redux

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -60,3 +60,35 @@ export const fetchLogOut = createAsyncThunk(
     }
   }
 );
+
+export const fetchUpdateUser = createAsyncThunk(
+  'auth/updateUser',
+  async (userData, thunkAPI) => {
+    const BASE_URL = 'https://aqua-track-project-back.onrender.com';
+    const END_POINT = '/auth/update-user';
+    const url = BASE_URL + END_POINT;
+
+    try {
+      const response = await axios.patch(url, userData);
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.response?.data || e.message);
+    }
+  }
+);
+
+export const fetchCurrentUser = createAsyncThunk(
+  'auth/currentUser',
+  async (_, thunkAPI) => {
+    const BASE_URL = 'https://aqua-track-project-back.onrender.com';
+    const END_POINT = '/auth/current-user';
+    const url = BASE_URL + END_POINT;
+
+    try {
+      const response = await axios.get(url);
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.response?.data || e.message);
+    }
+  }
+);
