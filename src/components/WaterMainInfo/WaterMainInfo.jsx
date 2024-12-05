0870c017888526809fc8044447587e7bbce6bbb1
olha_sydorchuk <157120293+Oolha@users.noreply.github.com>
2024-12-05 01:57:46 +0100

    try to fix directory to img for deploy

--- src/components/WaterMainInfo/WaterMainInfo.jsx
+++ src/components/WaterMainInfo/WaterMainInfo.jsx
@@ -1,5 +1,4 @@
 import css from './WaterMainInfo.module.css';
-
 import WaterDailyNorma from '../WaterDailyNorma/WaterDailyNorma';
 import Logo from '../Logo/Logo';
 import AddWaterBtn from '../AddWaterBtn/AddWaterBtn';
@@ -10,6 +9,13 @@ import { useModal } from '../../hooks/useModalHook.js';
 import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { fetchCurrentUser } from '../../redux/auth/operations.js';
+
+import bottleDesktop from '../../assets/images/bottle_desktop.png';
+import bottleDesktop2x from '../../assets/images/bottle_desktop@2x.png';
+import bottleTablet from '../../assets/images/bottle_tablet.png';
+import bottleTablet2x from '../../assets/images/bottle_tablet@2x.png';
+import bottleMobile from '../../assets/images/bottle_mobile.png';
+import bottleMobile2x from '../../assets/images/bottle_mobile@2x.png';
 
 const WaterMainInfo = () => {
   const [isOpenModal, setIsOpenModal] = useModal();
@@ -27,20 +33,20 @@ const WaterMainInfo = () => {
 
       <picture>
         <source
-          srcSet="/assets/images/bottle_desktop.png 1x, /assets/images/bottle_desktop@2x.png 2x"
+          srcSet={`${bottleDesktop} 1x, ${bottleDesktop2x} 2x`}
           media="(min-width: 1440px)"
         />
         <source
-          srcSet="/assets/images/bottle_tablet.png 1x, /assets/images/bottle_tablet@2x.png 2x"
+          srcSet={`${bottleTablet} 1x, ${bottleTablet2x} 2x`}
           media="(min-width: 768px)"
         />
         <source
-          srcSet="/assets/images/bottle_mobile.png 1x, /assets/images/bottle_mobile@2x.png 2x"
+          srcSet={`${bottleMobile} 1x, ${bottleMobile2x} 2x`}
           media="(max-width: 767px)"
         />
         <img
           className={css.imageBottle}
-          src="/assets/images/bottle_desktop.png"
+          src={bottleDesktop}
           alt="Transparent bottle for water"
         />
       </picture>
